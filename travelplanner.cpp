5bdce759a6770e3f1f7cb56c7f907f861d9cce88
charleskhu675 <charleskhu675@gmail.com>
2026-05-08 20:16:09 -0700

    structs for edge and node

--- travelplanner.cpp
+++ travelplanner.cpp
@@ -7,3 +7,27 @@
 #include <iomanip>
 
 using namespace std;
+
+const int INF = 1e9;
+
+struct Edge {
+    int to;
+    int weight;
+};
+
+struct Node {
+    int city;
+    int distance;
+    bool operator>(const Node& other) const {
+        return distance > other.distance;
+    }
+};
+
+class TravelPlanner {
+    private:
+        int numCities;
+        vector<vector<Edge>> adjList;
+
+    public:
+    
+}
